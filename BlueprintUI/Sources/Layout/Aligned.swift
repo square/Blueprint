e4cd64db4e9bfcd9c0886eae6b8031a548b02c3f
Andrew Watt <awatt@squareup.com>
2019-10-08 14:00:30 -0700

    Aligned element

---
+++ BlueprintUI/Sources/Layout/Aligned.swift
@@ -0,0 +1,99 @@
+/// Aligns a content element within itself. The vertical and horizontal alignment may be set independently.
+///
+/// The size of the content element is determined by calling `measure(in:)` on
+/// the content element – even if that size is larger than the wrapping element.
+///
+public struct Aligned: Element {
+    /// The possible vertical alignment values.
+    public enum VerticalAlignment {
+        /// Aligns the content to the top edge of the containing element.
+        case top
+        /// Centers the content vertically.
+        case center
+        /// Aligns the content to the bottom edge of the containing element.
+        case bottom
+    }
+
+    /// The possible horizontal alignment values.
+    public enum HorizontalAlignment {
+        /// Aligns the content to the leading edge of the containing element.
+        /// In left-to-right languages, this is the left edge.
+        case leading
+        /// Centers the content horizontally.
+        case center
+        /// Aligns the content to the trailing edge of the containing element.
+        /// In left-to-right languages, this is the right edge.
+        case trailing
+    }
+
+    /// The content element to be aligned.
+    public var wrappedElement: Element
+    /// The vertical alignment.
+    public var verticalAlignment: VerticalAlignment
+    /// The horizontal alignment.
+    public var horizontalAlignment: HorizontalAlignment
+
+    /// Initializes an `Aligned` with the given content element and alignments.
+    ///
+    /// - parameters:
+    ///   - vertically: The vertical alignment. Defaults to centered.
+    ///   - horizontally: The horizontal alignment. Defaults to centered.
+    ///   - wrapping: The content element to be aligned.
+    init(
+        vertically verticalAlignment: VerticalAlignment = .center,
+        horizontally horizontalAlignment: HorizontalAlignment = .center,
+        wrapping wrappedElement: Element
+    ) {
+        self.verticalAlignment = verticalAlignment
+        self.horizontalAlignment = horizontalAlignment
+        self.wrappedElement = wrappedElement
+    }
+
+    public var content: ElementContent {
+        let layout = Layout(verticalAlignment: verticalAlignment, horizontalAlignment: horizontalAlignment)
+        return ElementContent(child: wrappedElement, layout: layout)
+    }
+
+    public func backingViewDescription(bounds: CGRect, subtreeExtent: CGRect?) -> ViewDescription? {
+        return nil
+    }
+
+    private struct Layout: SingleChildLayout {
+        var verticalAlignment: VerticalAlignment
+        var horizontalAlignment: HorizontalAlignment
+
+        func measure(in constraint: SizeConstraint, child: Measurable) -> CGSize {
+            return child.measure(in: constraint)
+        }
+
+        func layout(size: CGSize, child: Measurable) -> LayoutAttributes {
+            let contentSize = child.measure(in: SizeConstraint(size))
+
+            var attributes = LayoutAttributes(size: contentSize)
+
+            switch verticalAlignment {
+            case .top:
+                attributes.frame.origin.y = 0
+            case .center:
+                attributes.frame.origin.y = (size.height - contentSize.height) / 2.0
+            case .bottom:
+                attributes.frame.origin.y = size.height - contentSize.height
+            }
+
+            switch horizontalAlignment {
+            case .leading:
+                attributes.frame.origin.x = 0
+            case .center:
+                attributes.frame.origin.x = (size.width - contentSize.width) / 2.0
+            case .trailing:
+                attributes.frame.origin.x = size.width - contentSize.width
+            }
+
+            // TODO: screen-scale round here once that lands
+            attributes.frame.origin.x.round()
+            attributes.frame.origin.y.round()
+
+            return attributes
+        }
+    }
+}
---
+++ BlueprintUI/Tests/AlignedTests.swift
@@ -0,0 +1,101 @@
+import XCTest
+@testable import BlueprintUI
+
+
+class AlignedTests: XCTestCase {
+
+    let testSize = CGSize(width: 100, height: 200)
+    let layoutFrame = CGRect(x: 0, y: 0, width: 5000, height: 6000)
+
+    private func childLayoutResultNodesAligned(
+        horizontally: Aligned.HorizontalAlignment = .center,
+        vertically: Aligned.VerticalAlignment = .center
+    ) -> [LayoutResultNode] {
+        let content = TestElement(size: testSize)
+        let element = Aligned(vertically: vertically, horizontally: horizontally, wrapping: content)
+        let children = element
+            .layout(frame: layoutFrame)
+            .children
+            .map { $0.node }
+        return children
+    }
+
+    func test_measuring() {
+        let constraint = SizeConstraint(width: .unconstrained, height: .unconstrained)
+        let content = TestElement(size: testSize)
+        let element = Aligned(wrapping: content)
+        XCTAssertEqual(element.content.measure(in: constraint), content.content.measure(in: constraint))
+    }
+
+    func test_horizontalLeading() {
+        let children = childLayoutResultNodesAligned(horizontally: .leading)
+
+        XCTAssertEqual(children.count, 1)
+        let frame = children[0].layoutAttributes.frame
+        XCTAssertEqual(frame.minX, 0)
+        XCTAssertEqual(frame.maxX, 100)
+        XCTAssertTrue(children[0].element is TestElement)
+    }
+
+    func test_horizontalCenter() {
+        let children = childLayoutResultNodesAligned(horizontally: .center)
+
+        XCTAssertEqual(children.count, 1)
+        let frame = children[0].layoutAttributes.frame
+        XCTAssertEqual(frame.minX, 2450)
+        XCTAssertEqual(frame.maxX, 2550)
+        XCTAssertTrue(children[0].element is TestElement)
+    }
+
+    func test_horizontalTrailing() {
+        let children = childLayoutResultNodesAligned(horizontally: .trailing)
+
+        XCTAssertEqual(children.count, 1)
+        let frame = children[0].layoutAttributes.frame
+        XCTAssertEqual(frame.minX, 4900)
+        XCTAssertEqual(frame.maxX, 5000)
+        XCTAssertTrue(children[0].element is TestElement)
+    }
+
+    func test_verticalTop() {
+        let children = childLayoutResultNodesAligned(vertically: .top)
+
+        XCTAssertEqual(children.count, 1)
+        let frame = children[0].layoutAttributes.frame
+        XCTAssertEqual(frame.minY, 0)
+        XCTAssertEqual(frame.maxY, 200)
+        XCTAssertTrue(children[0].element is TestElement)
+    }
+
+    func test_verticalCenter() {
+        let children = childLayoutResultNodesAligned(vertically: .center)
+
+        XCTAssertEqual(children.count, 1)
+        let frame = children[0].layoutAttributes.frame
+        XCTAssertEqual(frame.minY, 2900)
+        XCTAssertEqual(frame.maxY, 3100)
+        XCTAssertTrue(children[0].element is TestElement)
+    }
+
+    func test_verticalBottom() {
+        let children = childLayoutResultNodesAligned(vertically: .bottom)
+
+        XCTAssertEqual(children.count, 1)
+        let frame = children[0].layoutAttributes.frame
+        XCTAssertEqual(frame.minY, 5800)
+        XCTAssertEqual(frame.maxY, 6000)
+        XCTAssertTrue(children[0].element is TestElement)
+    }
+}
+
+private struct TestElement: Element {
+    let size: CGSize
+
+    var content: ElementContent {
+        return ElementContent(intrinsicSize: size)
+    }
+
+    func backingViewDescription(bounds: CGRect, subtreeExtent: CGRect?) -> ViewDescription? {
+        return nil
+    }
+}
