048cb1d2eb6f67542ab4f66e5b78f27fdded8f53
Kyle Van Essen <k@squareup.com>
2020-02-22 20:49:52 -0800

    self and code review

--- BlueprintUICommonControls/Sources/AttributedLabel.swift
+++ BlueprintUICommonControls/Sources/AttributedLabel.swift
@@ -1,53 +1,72 @@
 import BlueprintUI
 import UIKit
 
+// Displays attributed text in a label.
 public struct AttributedLabel: Element {
 
-    public var attributedText: NSAttributedString
+    /// The text shown within the label.
+    public var text: NSAttributedString
+    
+    /// The number of lines the label should display.
     public var numberOfLines: Int
+    
     /// The scale to which pixel measurements will be rounded. Defaults to `UIScreen.main.scale`.
     public var roundingScale: CGFloat = UIScreen.main.scale
 
-    public init(attributedText: NSAttributedString, numberOfLines: Int = 0) {
-        self.attributedText = attributedText
+    /// Creates a new label with the provided text and number of lines to display.
+    public init(
+        text: NSAttributedString,
+        numberOfLines: Int = 0
+    ) {
+        self.text = text
         self.numberOfLines = numberOfLines
     }
     
     /**
      Keep around a label to use for measurement and sizing.
      
-     We would usually do this using NSString or NSAttributedString's boundingRect family of methods,
-     but these do not let you specify a `numberOfLines` parameter, which is critical to correct sizing.
+     We would usually do this using `NSString` or `NSAttributedString's` `boundingRect` family of methods,
+     but these do not let you specify a `numberOfLines` parameter, which is critical to correct sizing. Further,
+     `UILabel`'s sizing is complex enough that we prefer to defer to its implementation, rather than atttempting
+     to re-implement it.
      
-     As such, we will allocate this label once and then use it to measure by setting its text and attributes.
+     As such, we will allocate this label once and then use it to measure by setting its text and attributes. A static
+     label is acceptable because layout code operates on the main thread.
      */
-    static let measurementLabel = UILabel()
+    private static let measurementLabel = UILabel()
 
     public var content: ElementContent {
-        
+        assert(Thread.isMainThread, "Expected sizing to occur on the main thread. This is required due to the use of `measurementLabel`.")
+
         return ElementContent { constraint in
             let label = AttributedLabel.measurementLabel
-            let description = self.backingViewDescription(bounds: CGRect(origin: .zero, size: constraint.maximum), subtreeExtent: nil)!
+            let fittingSize = constraint.maximum
+            
+            // Configure the measurement label to prepare it for measuring our text.
+            
+            let description = self.backingViewDescription(bounds: CGRect(origin: .zero, size: fittingSize), subtreeExtent: nil)!
             
             description.apply(to: label)
-            label.attributedText = self.attributedText
             
-            var size = label.sizeThatFits(constraint.maximum)
+            // Determine the returned size.
             
-            size.width = size.width.rounded(.up, by: self.roundingScale)
-            size.height = size.height.rounded(.up, by: self.roundingScale)
+            var size = label.sizeThatFits(fittingSize)
             
-            size.width = min(size.width, constraint.maximum.width, size.width)
+            // Constrain the returned size from the label to the
+            // constraint's maximum size. UILabel can return a size larger
+            // than the provided size, and we want to stay within the provided size.
+            
+            size.width = min(size.width, fittingSize.width)
+            size.height = min(size.height, fittingSize.height)
 
             return size
         }
     }
 
     public func backingViewDescription(bounds: CGRect, subtreeExtent: CGRect?) -> ViewDescription? {
-        return UILabel.describe { (config) in
-            config[\.attributedText] = attributedText
+        return UILabel.describe { config in
+            config[\.attributedText] = text
             config[\.numberOfLines] = numberOfLines
         }
     }
-
 }
--- BlueprintUICommonControls/Sources/Label.swift
+++ BlueprintUICommonControls/Sources/Label.swift
@@ -7,34 +7,73 @@ public struct Label : ProxyElement {
 
     /// The text to be displayed.
     public var text: String
-    public var font: UIFont = UIFont.systemFont(ofSize: UIFont.systemFontSize)
-    public var color: UIColor = .black
-    public var alignment: NSTextAlignment = .left
-    public var numberOfLines: Int = 0
-    public var lineBreakMode: NSLineBreakMode = .byWordWrapping
-    /// The scale to which pixel measurements will be rounded. Defaults to `UIScreen.main.scale`.
-    public var roundingScale: CGFloat = UIScreen.main.scale
+    
+    /// The font used to display the label.
+    public var font: UIFont
+    
+    /// The color of the label.
+    /// Defaults to black.
+    public var color: UIColor
+    
+    /// The alignment of the label.
+    /// Defaults to left alignment.
+    public var alignment: NSTextAlignment
+    
+    /// The max number of lines the label will render.
+    /// Defaults to zero, which is as many lines as needed.
+    public var numberOfLines: Int
+    
+    /// The line break mode for displaying text.
+    /// Defaults to word wrapping.
+    public var lineBreakMode: NSLineBreakMode
+    
+    //
+    // MARK: Initialization
+    //
 
-    public init(text: String, configure: (inout Label) -> Void = { _ in }) {
+    /// Creates a new label with the provided text and options.
+    /// You can further customize the label within the `configure` block.
+    public init(
+        text: String,
+        font: UIFont = UIFont.systemFont(ofSize: UIFont.systemFontSize),
+        color: UIColor = .black,
+        alignment: NSTextAlignment = .left,
+        numberOfLines: Int = 0,
+        lineBreakMode: NSLineBreakMode = .byWordWrapping,
+        configure: (inout Label) -> Void = { _ in }
+    ) {
         self.text = text
+        
+        self.font = font
+        self.color = color
+        self.alignment = alignment
+        self.numberOfLines = numberOfLines
+        self.lineBreakMode = lineBreakMode
+        
         configure(&self)
     }
     
+    //
+    // MARK: ProxyElement
+    //
+
+    public var elementRepresentation: Element {
+        return AttributedLabel(text: self.attributedText, numberOfLines: self.numberOfLines)
+    }
+    
     private var attributedText: NSAttributedString {
+        
         let paragraphStyle = NSParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
         paragraphStyle.alignment = alignment
         paragraphStyle.lineBreakMode = lineBreakMode
+        
         return NSAttributedString(
             string: text,
             attributes: [
                 NSAttributedString.Key.font: font,
                 NSAttributedString.Key.foregroundColor: color,
                 NSAttributedString.Key.paragraphStyle: paragraphStyle
-            ])
+            ]
+        )
     }
-
-    public var elementRepresentation: Element {
-        return AttributedLabel(attributedText: self.attributedText, numberOfLines: self.numberOfLines)
-    }
-
 }
