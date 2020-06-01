1570a76752973a62efe66ae9874da976b276cd26
Andrew Watt <awatt@squareup.com>
2020-06-01 14:21:36 -0700

    KeyPath-based EnvironmentAdapter

--- BlueprintUI/Sources/Environment/AdaptedEnvironment.swift
+++ BlueprintUI/Sources/Environment/AdaptedEnvironment.swift
@@ -31,6 +31,15 @@ public struct AdaptedEnvironment: Element {
         self.init(by: { $0[key] = value }, wrapping: child)
     }
 
+    /// Wraps an element with an environment that is modified for a single value.
+    /// - Parameters:
+    ///   - keyPath: The keypath of the environment value to modify.
+    ///   - value: The new environment value to cascade.
+    ///   - wrapping: The element to be wrapped.
+    public init<Value>(keyPath: WritableKeyPath<Environment, Value>, value: Value, wrapping child: Element) {
+        self.init(by: { $0[keyPath: keyPath] = value }, wrapping: child)
+    }
+
     public var content: ElementContent {
         return ElementContent(child: wrappedElement, environment: environmentAdapter)
     }
@@ -44,6 +53,11 @@ public extension Element {
     /// Wraps this element in an `AdaptedEnvironment` with the given enviroment key and value.
     func adaptedEnvironment<Key>(key: Key.Type, value: Key.Value) -> Element where Key: EnvironmentKey {
         AdaptedEnvironment(key: key, value: value, wrapping: self)
+    }
+
+    /// Wraps this element in an `AdaptedEnvironment` with the given keypath and value.
+    func adaptedEnvironment<Value>(keyPath: WritableKeyPath<Environment, Value>, value: Value) -> Element {
+        AdaptedEnvironment(keyPath: keyPath, value: value, wrapping: self)
     }
 
     /// Wraps this element in an `AdaptedEnvironment` with the given configuration block.
