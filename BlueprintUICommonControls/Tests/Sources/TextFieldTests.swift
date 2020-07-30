d23e7b7ddaf041f1f8e64dc1adc84cbb846401d8
Christian Le <cle@squareup.com>
2020-07-29 17:27:11 -0700

    Expose setting font for TextField

--- BlueprintUICommonControls/Tests/Sources/TextFieldTests.swift
+++ BlueprintUICommonControls/Tests/Sources/TextFieldTests.swift
@@ -42,6 +42,15 @@ class TextFieldTests: XCTestCase {
                 identifier: "right-aligned")
         }
 
+        do {
+            var field = TextField(text: "Title font")
+            field.font = .preferredFont(forTextStyle: .title1)
+            compareSnapshot(
+                of: field,
+                size: CGSize(width: 200, height: 44),
+                identifier: "title-font")
+        }
+
     }
 
 }
