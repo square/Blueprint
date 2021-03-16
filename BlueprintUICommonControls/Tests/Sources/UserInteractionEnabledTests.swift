a318dd8dfa0342f74a120239e2551812071093a3
Noah Blake <noahb@squareup.com>
2021-03-16 10:48:25 -0400

    [Gardening] Expand test coverage to convenience methods

--- BlueprintUICommonControls/Tests/Sources/UserInteractionEnabledTests.swift
+++ BlueprintUICommonControls/Tests/Sources/UserInteractionEnabledTests.swift
@@ -11,16 +11,35 @@ import BlueprintUICommonControls
 
 final class UserInteractionEnabledTests: XCTestCase {
     func test() throws {
-        func makeView(enabled: Bool) throws -> UIView {
-            let wrapped = UserInteractionEnabled(enabled, wrapping: Box())
-            let description = try XCTUnwrap(wrapped.backingViewDescription(bounds: .zero, subtreeExtent: nil))
-            let view = UIView()
-            view.isUserInteractionEnabled = !enabled
-            description.apply(to: view)
-            return view
-        }
+        let enabled = UserInteractionEnabled(true, wrapping: Box())
+        XCTAssertTrue(try view(from: enabled).isUserInteractionEnabled)
 
-        XCTAssertTrue(try makeView(enabled: true).isUserInteractionEnabled)
-        XCTAssertFalse(try makeView(enabled: false).isUserInteractionEnabled)
+        let disabled = UserInteractionEnabled(false, wrapping: Box())
+        XCTAssertFalse(try view(from: disabled).isUserInteractionEnabled)
+    }
+
+    func test_disabled() throws {
+        let enabled = Box().disabled(false)
+        XCTAssertTrue(try view(from: enabled).isUserInteractionEnabled)
+
+        let disabled = Box().disabled()
+        XCTAssertFalse(try view(from: disabled).isUserInteractionEnabled)
+    }
+
+    func test_enabled() throws {
+        let enabled = Box().enabled()
+        XCTAssertTrue(try view(from: enabled).isUserInteractionEnabled)
+
+        let disabled =  Box().enabled(false)
+        XCTAssertFalse(try view(from: disabled).isUserInteractionEnabled)
+    }
+
+    // MARK: - helpers -
+    func view(from element: UserInteractionEnabled) throws -> UIView {
+        let description = try XCTUnwrap(element.backingViewDescription(bounds: .zero, subtreeExtent: nil))
+        let view = UIView()
+        view.isUserInteractionEnabled = !element.isEnabled
+        description.apply(to: view)
+        return view
     }
 }
