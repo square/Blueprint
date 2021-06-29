07f07b16629b7cdd106f65374229ff375eb76011
Kyle Van Essen <k@squareup.com>
2021-06-28 20:22:17 -0700

    Collapse nested AdaptedEnvironments

--- BlueprintUI/Sources/Environment/AdaptedEnvironment.swift
+++ BlueprintUI/Sources/Environment/AdaptedEnvironment.swift
@@ -6,20 +6,30 @@ import CoreGraphics
 /// will automatically inherit those values automatically. Values can be changed
 /// anywhere in a sub-tree by inserting another `AdaptedEnvironment` element.
 public struct AdaptedEnvironment: Element {
-    var wrappedElement: Element
-    var environmentAdapter: (inout Environment) -> Void
+    
+    /// Takes in a mutable `Environment` which can be mutated to add or override values.
+    public typealias Adapter = (inout Environment) -> Void
+    
+    var wrapped: Element
+    var adapters: [Adapter]
 
     /// Wraps an element with an environment that is modified using the given
     /// configuration block.
+    ///
     /// - Parameters:
     ///   - by: A block that will set environmental values.
     ///   - wrapping: The element to be wrapped.
     public init(
-        by environmentAdapter: @escaping (inout Environment) -> Void,
-        wrapping wrappedElement: Element)
-    {
-        self.wrappedElement = wrappedElement
-        self.environmentAdapter = environmentAdapter
+        by adapt: @escaping Adapter,
+        wrapping wrapped: Element
+    ) {
+        if var adapter = wrapped as? AdaptedEnvironment {
+            adapter.adapters.append(adapt)
+            self = adapter
+        } else {
+            self.wrapped = wrapped
+            self.adapters = [adapt]
+        }
     }
 
     /// Wraps an element with an environment that is modified for a single key and value.
@@ -27,7 +37,12 @@ public struct AdaptedEnvironment: Element {
     ///   - key: The environment key to modify.
     ///   - value: The new environment value to cascade.
     ///   - wrapping: The element to be wrapped.
-    public init<Key>(key: Key.Type, value: Key.Value, wrapping child: Element) where Key: EnvironmentKey {
+    public init<Key>(
+        key: Key.Type,
+        value: Key.Value,
+        wrapping child: Element
+    ) where Key: EnvironmentKey
+    {
         self.init(by: { $0[key] = value }, wrapping: child)
     }
 
@@ -36,21 +51,34 @@ public struct AdaptedEnvironment: Element {
     ///   - keyPath: The keypath of the environment value to modify.
     ///   - value: The new environment value to cascade.
     ///   - wrapping: The element to be wrapped.
-    public init<Value>(keyPath: WritableKeyPath<Environment, Value>, value: Value, wrapping child: Element) {
+    public init<Value>(
+        keyPath: WritableKeyPath<Environment, Value>,
+        value: Value,
+        wrapping child: Element
+    ) {
         self.init(by: { $0[keyPath: keyPath] = value }, wrapping: child)
     }
 
     public var content: ElementContent {
-        return ElementContent(child: wrappedElement, environment: environmentAdapter)
+        ElementContent(
+            child: wrapped,
+            environment: { env in
+                for adapter in self.adapters.reversed() {
+                    adapter(&env)
+                }
+            }
+        )
     }
 
     public func backingViewDescription(with context: ViewDescriptionContext) -> ViewDescription? {
-        return nil
+        nil
     }
 }
 
+
 public extension Element {
-    /// Wraps this element in an `AdaptedEnvironment` with the given enviroment key and value.
+    
+    /// Wraps this element in an `AdaptedEnvironment` with the given environment key and value.
     func adaptedEnvironment<Key>(key: Key.Type, value: Key.Value) -> Element where Key: EnvironmentKey {
         AdaptedEnvironment(key: key, value: value, wrapping: self)
     }
