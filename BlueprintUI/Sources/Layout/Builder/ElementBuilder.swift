33f0dfd41cf0515ea53bfa9e6ed9feba3767123d
Luke Street <lstreet@squareup.com>
2021-08-23 11:52:58 -0400

    Remove element container and instead use buildExpression and buildFinalResult to clean up logic.

--- BlueprintUI/Sources/Layout/Builder/ElementBuilder.swift
+++ BlueprintUI/Sources/Layout/Builder/ElementBuilder.swift
@@ -1,35 +1,29 @@
 /// Generic result builder for converting blocks of `Child...` into `[Child]`.
 @_functionBuilder // change to @resultBuilder when we upgrade to Swift 5.4.
 public struct ElementBuilder {
-    /// Function builder for converting blocks of `Child...` into `[Child]`.
-    /// - Parameter children: All children.
-    /// - Returns: `[Child]`.
-    public static func buildBlock(_ elements: Element...) -> [Element] {
-        elements.map { element -> [Element] in
-            if let many = element as? ElementContainer {
-                return many.elements
-            } else {
-                return [element]
-            }
-        }.flatMap { $0 }
+    public typealias Component = [Element]
+
+    public static func buildBlock(_ elements: Component...) -> Component {
+        elements.flatMap { $0 }
     }
 
-    public static func buildOptional(_ elements: [Element]?) -> Element {
-        ElementContainer(elements: elements ?? [])
+    public static func buildOptional(_ elements: Component?) -> Component {
+        elements ?? []
     }
 
-    public static func buildEither(first: [Element]) -> Element {
-        ElementContainer(elements: first)
+    public static func buildEither(first: Component) -> Component {
+        first
     }
 
-    public static func buildEither(second: [Element]) -> Element {
-        ElementContainer(elements: second)
+    public static func buildEither(second: Component) -> Component {
+        second
     }
-}
 
-private struct ElementContainer: ProxyElement {
-    var elements: [Element]
-    var elementRepresentation: Element {
-        fatalError("This type should never render. It should only be used to contain other Elements.")
+    public static func buildExpression(_ element: Element) -> Component {
+        [element]
+    }
+
+    public static func buildExpression(_ element: [Element]) -> Component {
+        element
     }
 }
