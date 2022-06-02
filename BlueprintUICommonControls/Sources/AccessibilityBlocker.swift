f16d1792b3c50f42e1c6ab422689fc7af68c8326
Kyle Van Essen <k@squareup.com>
2022-06-02 08:45:27 -0700

    Allow AccessibilityBlocker to take in an isBlocking parameter.

--- BlueprintUICommonControls/Sources/AccessibilityBlocker.swift
+++ BlueprintUICommonControls/Sources/AccessibilityBlocker.swift
@@ -7,10 +7,19 @@ import UIKit
 /// hidden from the accessibility system.
 public struct AccessibilityBlocker: Element {
 
+    /// The element whose accessibility information will be blocked.
     public var wrapped: Element
 
+    /// If the `AccessibilityBlocker` is currently blocking accessibility.
+    public var isBlocking: Bool
+
     /// Creates a new `AccessibilityBlocker` wrapping the provided element.
-    public init(wrapping element: Element) {
+    public init(
+        isBlocking: Bool = true,
+        wrapping element: Element
+    ) {
+        self.isBlocking = isBlocking
+
         wrapped = element
     }
 
@@ -24,8 +33,8 @@ public struct AccessibilityBlocker: Element {
 
     public func backingViewDescription(with context: ViewDescriptionContext) -> ViewDescription? {
         UIView.describe { config in
-            config[\.isAccessibilityElement] = false
-            config[\.accessibilityElementsHidden] = true
+            config[\.isAccessibilityElement] = !isBlocking
+            config[\.accessibilityElementsHidden] = isBlocking
         }
     }
 }
@@ -36,7 +45,7 @@ extension Element {
     /// Blocks all accessibility on the element, so that it is
     /// is no longer an accessibility element, and its children are
     /// hidden from the accessibility system.
-    public func blockAccessibility() -> AccessibilityBlocker {
-        AccessibilityBlocker(wrapping: self)
+    public func blockAccessibility(isBlocking: Bool = true) -> AccessibilityBlocker {
+        AccessibilityBlocker(isBlocking: isBlocking, wrapping: self)
     }
 }
