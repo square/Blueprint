95fae6eb01226e7f5c1c2fb16cefb38b00e7c072
Kyle Van Essen <k@squareup.com>
2022-08-15 16:23:54 -0700

    Bail early from logging methods if logging is disabled

--- BlueprintUI/Sources/Internal/OSLog+Blueprint.swift
+++ BlueprintUI/Sources/Internal/OSLog+Blueprint.swift
@@ -6,16 +6,17 @@ extension OSLog {
     static let blueprint = OSLog(subsystem: "com.squareup.Blueprint", category: "Blueprint")
 
     /// The log to be used for `os_log` and `os_signpost` logging.
-    static var active: OSLog = OSLog.disabled
+    fileprivate(set) static var active: OSLog = OSLog.disabled
 }
 
 /// Namespace for logging config.
 public enum BlueprintLogging {
+
     /// If enabled, Blueprint will emit signpost logs. You can view these logs in Instruments to
     /// aid in debugging or performance tuning.
     ///
     /// Signpost logging is disabled by default.
-    public static var enabled: Bool {
+    public static var isEnabled: Bool {
         get {
             OSLog.active === OSLog.blueprint
         }
