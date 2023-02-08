08e7068cc7f2516d211a22f7ddcc53779bc7e2e7
Andrew Watt <awatt@squareup.com>
2023-02-07 21:29:10 -0800

    shorthand enum case

--- BlueprintUI/Sources/Layout/LayoutMode.swift
+++ BlueprintUI/Sources/Layout/LayoutMode.swift
@@ -6,4 +6,6 @@ public enum LayoutMode: Equatable {
     case standard
     case singlePass(options: SPCacheOptions = .default)
     case strictSinglePass
+    
+    public static let singlePass: Self = .singlePass()
 }
