7c5c6b7133fd6efe2fc21557af01acb1825df5ca
Andrew Watt <100192+watt@users.noreply.github.com>
2023-02-23 12:39:57 -0800

    Xcode 14 (#432)
    * update to xcode 14
    
    * update swiftformat
    
    * add ios 16 to test matrix
    
    * delete ios 12 & 13 snapshot references
    
    * update to macos-12
    
    * switch from xcversion to xcodes
    
    * install xcodes
    
    * xcodes needs sudo
    
    * update cocoapods sim target
    
    * update ios 14.5 sim target
    
    * disable performance playground
    
    * fix snapshot archive path
    
    * use a 2x device on ios 14.5
    
    * persist spm generated schemes
    
    * imperceptible ci snapshot differences
    
    * speed up xcodes with aria2
    
    * document gitignore change

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,7 +20,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.48.11"),
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.50.8"),
     ],
     targets: [
         .target(
