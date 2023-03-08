4a00dc44162e494954a7a7c953ed91c1c45442b0
Andrew Watt <awatt@squareup.com>
2023-03-07 16:17:23 -0800

    Contract-based cache optimizations

--- BlueprintUI/Sources/Internal/HintingSizeCache.swift
+++ BlueprintUI/Sources/Internal/HintingSizeCache.swift
@@ -2,6 +2,9 @@ import CoreGraphics
 
 /// A measurement cache that automatically "hints" values that can be deduced from the Caffeinated
 /// Layout contract.
+///
+/// See the documentation for more details about the layout contract and hinting optimizations.
+///
 final class HintingSizeCache {
 
     typealias Options = LayoutOptions
@@ -9,6 +12,8 @@ final class HintingSizeCache {
     typealias Value = CGSize
 
     private var values: [Key: Value] = [:]
+    private var hasUnconstrainedWidth: Bool = false
+    private var hasUnconstrainedHeight: Bool = false
 
     let path: String
     let signpostRef: AnyObject
@@ -26,7 +31,59 @@ final class HintingSizeCache {
             return size
         }
 
-        // TODO: implement hinting
+        // This option allows us to deduce if the key is within a hinted range even if it does not
+        // land on a boundary value, by searching "well known" keys.
+        if options.searchUnconstrainedKeys {
+
+            // When there are no unconstrained keys, searching for them here can have a significant
+            // performance hit. Unconstrained keys are usually not present unless introduced by an
+            // element like a ScrollView.
+            //
+            // To avoid this hit, we track whether we have a seen an unconstrained key in either
+            // axis, and check that before attempting a search.
+
+            defer {
+                // We only need to track the explicit lookup key. The hinted keys from
+                // deduced range boundaries will never introduce a new unconstrained key.
+
+                hasUnconstrainedWidth = hasUnconstrainedWidth || key.width == .unconstrained
+                hasUnconstrainedHeight = hasUnconstrainedHeight || key.height == .unconstrained
+            }
+
+            if
+                hasUnconstrainedHeight,
+                case .atMost = key.width,
+                case .atMost(let maxHeight) = key.height,
+                let size = values[.init(width: key.width, height: .unconstrained)],
+                size.height <= maxHeight
+            {
+                Logger.logCacheUnconstrainedSearchMatch(
+                    object: signpostRef,
+                    description: path,
+                    constraint: key,
+                    match: .init(width: key.width, height: .unconstrained)
+                )
+                values[key] = size
+                return size
+            }
+
+            if
+                hasUnconstrainedWidth,
+                case .atMost = key.height,
+                case .atMost(let maxWidth) = key.width,
+                let size = values[.init(width: .unconstrained, height: key.height)],
+                size.width <= maxWidth
+            {
+                Logger.logCacheUnconstrainedSearchMatch(
+                    object: signpostRef,
+                    description: path,
+                    constraint: key,
+                    match: .init(width: .unconstrained, height: key.height)
+                )
+                values[key] = size
+                return size
+            }
+        }
 
         // This particular log has a small, but non-negligible impact on a certain class of test
         // cases, such as deeply nested stacks. Enable it manually if you want this statistic.
@@ -37,6 +94,47 @@ final class HintingSizeCache {
         Logger.logMeasureEnd(object: signpostRef)
 
         values[key] = size
+
+        // This option adds (or "hints") extra cache keys along the boundaries of the range
+        // between the constraint and the measured size.
+        if options.hintRangeBoundaries {
+
+            func hint(key: SizeConstraint) {
+                values[key] = size
+            }
+
+            switch (key.width, key.height) {
+            case (.unconstrained, .unconstrained):
+                hint(key: SizeConstraint(width: .unconstrained, height: .atMost(size.height)))
+                hint(key: SizeConstraint(width: .atMost(size.width), height: .unconstrained))
+                hint(key: SizeConstraint(size))
+
+            case (.unconstrained, .atMost(let maxHeight)):
+                if size.height < maxHeight {
+                    hint(key: SizeConstraint(width: .unconstrained, height: .atMost(size.height)))
+                    hint(key: SizeConstraint(size))
+                }
+                hint(key: SizeConstraint(width: .atMost(size.width), height: key.height))
+
+            case (.atMost(let maxWidth), .unconstrained):
+                if size.width < maxWidth {
+                    hint(key: SizeConstraint(width: .atMost(size.width), height: .unconstrained))
+                    hint(key: SizeConstraint(size))
+                }
+                hint(key: SizeConstraint(width: key.width, height: .atMost(size.height)))
+
+            case (.atMost(let maxWidth), .atMost(let maxHeight)):
+                if size.width < maxWidth {
+                    hint(key: SizeConstraint(width: .atMost(size.width), height: key.height))
+                }
+                if size.height < maxHeight {
+                    hint(key: SizeConstraint(width: key.width, height: .atMost(size.height)))
+                }
+                if size.width < maxWidth && size.height < maxHeight {
+                    hint(key: SizeConstraint(size))
+                }
+            }
+        }
 
         return size
     }
---
+++ BlueprintUI/Tests/HintingSizeCacheTests.swift
@@ -0,0 +1,166 @@
+import XCTest
+@testable import BlueprintUI
+
+final class HintingSizeCacheTests: XCTestCase {
+
+    func test_caching() {
+
+        func test(key: SizeConstraint) {
+            let size = CGSize(width: 100, height: 100)
+
+            let boundaries = key.boundaries(betweenSelfAndSize: size)
+            // test all the keys twice
+            let keys = boundaries + boundaries
+
+            assertMisses(
+                keys: keys,
+                size: size,
+                // we expect to hit each of the unique keys exactly once
+                misses: boundaries,
+                options: .init(hintRangeBoundaries: false, searchUnconstrainedKeys: false)
+            )
+        }
+
+        test(key: SizeConstraint(width: .atMost(200), height: .atMost(200)))
+        test(key: SizeConstraint(width: .atMost(200), height: .unconstrained))
+        test(key: SizeConstraint(width: .unconstrained, height: .atMost(200)))
+        test(key: .unconstrained)
+    }
+
+    func test_hintRangeBoundaries() throws {
+
+        func test(key: SizeConstraint) {
+            let size = CGSize(width: 100, height: 100)
+
+            let boundaries = key.boundaries(betweenSelfAndSize: size)
+            let offKeys = [
+                SizeConstraint(width: key.width, height: .atMost(150)),
+                SizeConstraint(width: .atMost(150), height: key.height),
+            ]
+            // test all the keys twice
+            let keys = boundaries + boundaries + offKeys + offKeys
+
+            assertMisses(
+                keys: keys,
+                size: size,
+                // we expect to miss the first key, deduce the other boundaries, and miss the off keys
+                misses: [key] + offKeys,
+                options: .init(hintRangeBoundaries: true, searchUnconstrainedKeys: false)
+            )
+        }
+
+        test(key: SizeConstraint(width: .atMost(200), height: .atMost(200)))
+        test(key: SizeConstraint(width: .atMost(200), height: .unconstrained))
+        test(key: SizeConstraint(width: .unconstrained, height: .atMost(200)))
+        test(key: .unconstrained)
+    }
+
+    func test_searchUnconstrainedKeys() {
+
+        let size = CGSize(width: 100, height: 100)
+
+        assertMisses(
+            keys: [
+                SizeConstraint(width: .atMost(200), height: .unconstrained),
+                SizeConstraint(width: .atMost(200), height: .atMost(200)),
+            ],
+            size: size,
+            // we expect to hit only the first key, and range-match the second
+            misses: [SizeConstraint(width: .atMost(200), height: .unconstrained)],
+            options: .init(hintRangeBoundaries: false, searchUnconstrainedKeys: true)
+        )
+
+        assertMisses(
+            keys: [
+                SizeConstraint(width: .unconstrained, height: .atMost(200)),
+                SizeConstraint(width: .atMost(200), height: .atMost(200)),
+            ],
+            size: size,
+            // we expect to hit only the first key, and range-match the second
+            misses: [SizeConstraint(width: .unconstrained, height: .atMost(200))],
+            options: .init(hintRangeBoundaries: false, searchUnconstrainedKeys: true)
+        )
+
+        let keys = [
+            .unconstrained,
+            SizeConstraint(width: .atMost(200), height: .atMost(200)),
+            SizeConstraint(width: .unconstrained, height: .atMost(200)),
+            SizeConstraint(width: .atMost(200), height: .unconstrained),
+        ]
+        assertMisses(
+            keys: keys,
+            size: size,
+            // we do not search the double-unconstrained key, so these are all misses
+            misses: keys,
+            options: .init(hintRangeBoundaries: false, searchUnconstrainedKeys: true)
+        )
+    }
+
+    func test_hintRangeBoundariesAndSearchUnconstrained() {
+        let size = CGSize(width: 100, height: 100)
+
+        assertMisses(
+            keys: [
+                .unconstrained,
+                SizeConstraint(width: .atMost(100), height: .atMost(150)),
+                SizeConstraint(width: .atMost(150), height: .atMost(100)),
+            ],
+            size: size,
+            // we will miss the first key, but can then range-match the others off of hinted boundary keys
+            misses: [.unconstrained],
+            options: .init(hintRangeBoundaries: true, searchUnconstrainedKeys: true)
+        )
+    }
+
+    /// Assert that a `HintingSizeCache` misses on the expected keys.
+    ///
+    /// - Parameters:
+    ///   - keys: Keys to probe the cache with, in order. The expected misses are probably always a subset of these.
+    ///   - size: A fixed size that the measuring function should return to the cache.
+    ///   - expectedMisses: The keys that are expected to generate a cache miss and call the measurer.
+    ///   - options: Options for the cache under test.
+    private func assertMisses(
+        keys: [SizeConstraint],
+        size: CGSize,
+        misses expectedMisses: [SizeConstraint],
+        options: LayoutOptions,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        let cache = HintingSizeCache(
+            path: "test",
+            signpostRef: SignpostToken(),
+            options: options
+        )
+
+        var misses: [SizeConstraint] = []
+
+        func measure(_ constraint: SizeConstraint) -> CGSize {
+            misses.append(constraint)
+            return size
+        }
+
+        for key in keys {
+            XCTAssertEqual(
+                cache.get(key: key, or: measure),
+                size,
+                file: file,
+                line: line
+            )
+        }
+
+        XCTAssertEqual(misses, expectedMisses, file: file, line: line)
+    }
+}
+
+extension SizeConstraint {
+    /// Generate constraints with every permutation of width & height between `self` and the given size.
+    func boundaries(betweenSelfAndSize size: CGSize) -> [SizeConstraint] {
+        [
+            self,
+            .init(width: width, height: .atMost(size.height)),
+            .init(width: .atMost(size.width), height: height),
+            .init(size),
+        ]
+    }
+}
