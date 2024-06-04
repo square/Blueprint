591034ec12cdc3704e6c0aa4443e732710614f14
Justin Hatin <jhatin@squareup.com>
2024-06-03 20:02:43 -0400

    Uses maskedCorners property to write cornerMask to

--- BlueprintUICommonControls/Sources/Box.swift
+++ BlueprintUICommonControls/Sources/Box.swift
@@ -113,8 +113,8 @@ public struct Box: Element {
                     view.contentView.layer.cornerRadius = self.cornerStyle.radius(for: context.bounds)
                 }
 
-                if self.cornerStyle.cornerMask != view.contentView.layer.cornerMask {
-                    view.contentView.layer.cornerMask = self.cornerStyle.cornerMask
+                if self.cornerStyle.cornerMask != view.contentView.layer.maskedCorners {
+                    view.contentView.layer.maskedCorners = self.cornerStyle.cornerMask
                 }
             }
 
