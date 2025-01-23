8c0ccaf57500953796bcba26948e75cd6173d2be
Alex Odawa <aodawa@squareup.com>
2025-01-23 19:36:23 +0100

    now returning nil rather than clamping the index

--- BlueprintUI/Sources/Extensions/BidirectionalCollection+Blueprint.swift
+++ BlueprintUI/Sources/Extensions/BidirectionalCollection+Blueprint.swift
@@ -10,19 +10,12 @@ extension BidirectionalCollection where Element: Equatable, Element: NSObjectPro
             return UIAccessibilityCustomRotorItemResult(targetElement: first, targetRange: nil)
         }
         let newIndex = (predicate.searchDirection == .next ? index(after: currentIndex) : index(before: currentIndex))
-            .clamped(min: startIndex, max: index(before: endIndex))
+        guard newIndex >= startIndex, newIndex < endIndex else { return nil }
         return .init(targetElement: self[newIndex], targetRange: nil)
     }
 
     /// Returns a UIAccessibilityCustomRotor with the provided system type which cycles through the contained elements.
     public func accessibilityRotor(systemType type: UIAccessibilityCustomRotor.SystemRotorType) -> UIAccessibilityCustomRotor {
         UIAccessibilityCustomRotor(systemType: type) { itemSearch($0) }
-    }
-}
-
-// MARK: Misc. Internal
-extension Comparable {
-    fileprivate func clamped(min: Self, max: Self) -> Self {
-        Swift.max(Swift.min(self, max), min)
     }
 }
