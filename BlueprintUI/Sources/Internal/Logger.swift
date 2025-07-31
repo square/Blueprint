84fe9bbdbc9f456317deeab558df5931623715c2
Max Goedjen <maxg@squareup.com>
2025-07-31 12:12:59 -0700

    Squash

--- BlueprintUI/Sources/Internal/Logger.swift
+++ BlueprintUI/Sources/Internal/Logger.swift
@@ -2,11 +2,15 @@ import Foundation
 import os.log
 
 /// Namespace for logging helpers
-enum Logger {}
+enum Logger {
+    fileprivate static let signposter = OSSignposter(logHandle: .active)
+    static var hook: ((String) -> Void)?
+}
 
-/// BlueprintView signposts
+// MARK: - BlueprintView signposts
 extension Logger {
     static func logLayoutStart(view: BlueprintView) {
+
 
         guard BlueprintLogging.isEnabled else { return }
 
@@ -100,7 +104,8 @@ extension Logger {
     }
 }
 
-/// Measuring signposts
+// MARK: - HintingSizeCache signposts
+
 extension Logger {
     static func logMeasureStart(object: AnyObject, description: String, constraint: SizeConstraint) {
 
@@ -185,10 +190,169 @@ extension Logger {
         )
     }
 
-    // MARK: Utilities
 
+
+}
+
+// MARK: - CacheStorage
+
+extension Logger {
+
+    // MARK: Environment Comparison
+
+    static func logEnvironmentKeySetEquivalencyComparisonStart(key: some Hashable) -> OSSignpostIntervalState? {
+        guard BlueprintLogging.isEnabled else { return nil }
+        let token = signposter.beginInterval(
+            "Environment key set equivalency comparison",
+            id: key.signpost,
+            "Start: \(String(describing: key))"
+        )
+        hook?("\(#function) \(String(describing: key))")
+        return token
+    }
+
+    static func logEnvironmentKeySetEquivalencyComparisonEnd(_ token: OSSignpostIntervalState?, key: some Hashable) {
+        guard BlueprintLogging.isEnabled, let token else { return }
+        signposter.endInterval("Environment key set equivalency comparison", token, "\(String(describing: key))")
+        hook?("\(#function) \(String(describing: key))")
+    }
+
+    static func logEnvironmentEquivalencyComparisonStart(environment: Environment) -> OSSignpostIntervalState? {
+        guard BlueprintLogging.isEnabled else { return nil }
+        let token = signposter.beginInterval(
+            "Environment equivalency comparison",
+            id: environment.fingerprint.value.signpost,
+            "Start: \(String(describing: environment))"
+        )
+        hook?("\(#function) \(environment.fingerprint)")
+        return token
+    }
+
+    static func logEnvironmentEquivalencyComparisonEnd(_ token: OSSignpostIntervalState?, environment: Environment) {
+        guard BlueprintLogging.isEnabled, let token else { return }
+        signposter.endInterval("Environment equivalency comparison", token, "\(String(describing: environment))")
+        hook?("\(#function) \(environment.fingerprint)")
+    }
+
+    static func logEnvironmentEquivalencyFingerprintEqual(environment: Environment) {
+        guard BlueprintLogging.isEnabled else { return }
+        signposter.emitEvent("Environments trivially equal from fingerprint", id: environment.fingerprint.value.signpost)
+        hook?("\(#function) \(environment.fingerprint)")
+    }
+
+    static func logEnvironmentEquivalencyFingerprintCacheHit(environment: Environment) {
+        guard BlueprintLogging.isEnabled else { return }
+        signposter.emitEvent("Environment cached comparison result hit", id: environment.fingerprint.value.signpost)
+        hook?("\(#function) \(environment.fingerprint)")
+    }
+
+    static func logEnvironmentEquivalencyFingerprintCacheMiss(environment: Environment) {
+        guard BlueprintLogging.isEnabled else { return }
+        signposter.emitEvent("Environment cached comparison result miss", id: environment.fingerprint.value.signpost)
+        hook?("\(#function) \(environment.fingerprint)")
+    }
+
+    static func logEnvironmentEquivalencyCompletedWithNonEquivalence(
+        environment: Environment,
+        key: some Hashable,
+        context: EquivalencyContext
+    ) {
+        guard BlueprintLogging.isEnabled else { return }
+        signposter.emitEvent(
+            "Environment equivalency completed with non-equivalent result",
+            id: environment.fingerprint.value.signpost,
+            "\(String(describing: context)): \(String(describing: key)) not equivalent"
+        )
+        hook?("\(#function) \(String(describing: key))")
+    }
+
+    static func logEnvironmentEquivalencyCompletedWithEquivalence(environment: Environment, context: EquivalencyContext) {
+        guard BlueprintLogging.isEnabled else { return }
+        signposter.emitEvent(
+            "Environment equivalency completed with equivalent result",
+            id: environment.fingerprint.value.signpost,
+            "\(String(describing: context))"
+        )
+        hook?("\(#function) \(environment.fingerprint)")
+    }
+
+
+    // MARK: ValidatingCache
+
+    static func logValidatingCacheValidationStart(key: some Hashable) -> OSSignpostIntervalState? {
+        guard BlueprintLogging.isEnabled else { return nil }
+        let token = signposter.beginInterval(
+            "ValidatingCache validation",
+            id: key.signpost,
+            "Start: \(String(describing: key))"
+        )
+        hook?("\(#function) \(String(describing: key))")
+        return token
+    }
+
+    static func logValidatingCacheValidationEnd(_ token: OSSignpostIntervalState?, key: some Hashable) {
+        guard BlueprintLogging.isEnabled, let token else { return }
+        signposter.endInterval("ValidatingCache validation", token, "\(String(describing: key))")
+        hook?("\(#function) \(String(describing: key))")
+    }
+
+    static func logValidatingCacheFreshValueCreationStart(key: some Hashable) -> OSSignpostIntervalState? {
+        guard BlueprintLogging.isEnabled else { return nil }
+        let token = signposter.beginInterval(
+            "ValidatingCache fresh value creation",
+            id: key.signpost,
+            "\(String(describing: key))"
+        )
+        hook?("\(#function) \(String(describing: key))")
+        return token
+    }
+
+    static func logValidatingCacheFreshValueCreationEnd(_ token: OSSignpostIntervalState?, key: some Hashable) {
+        guard BlueprintLogging.isEnabled, let token else { return }
+        signposter.endInterval("ValidatingCache fresh value creation", token)
+        hook?("\(#function) \(String(describing: key))")
+    }
+
+    static func logValidatingCacheKeyMiss(key: some Hashable) {
+        guard BlueprintLogging.isEnabled else { return }
+        signposter.emitEvent("ValidatingCache key miss", id: key.signpost)
+        hook?("\(#function) \(String(describing: key))")
+    }
+
+    static func logValidatingCacheKeyHit(key: some Hashable) {
+        guard BlueprintLogging.isEnabled else { return }
+        signposter.emitEvent("ValidatingCache key hit", id: key.signpost)
+        hook?("\(#function) \(String(describing: key))")
+    }
+
+    static func logValidatingCacheHitAndValidationSuccess(key: some Hashable) {
+        guard BlueprintLogging.isEnabled else { return }
+        signposter.emitEvent("ValidatingCache validation success", id: key.signpost)
+        hook?("\(#function) \(String(describing: key))")
+    }
+
+    static func logValidatingCacheHitAndValidationFailure(key: some Hashable) {
+        guard BlueprintLogging.isEnabled else { return }
+        signposter.emitEvent("ValidatingCache validation failure", id: key.signpost)
+        hook?("\(#function) \(String(describing: key))")
+    }
+
+}
+
+extension Hashable {
+
+    fileprivate var signpost: OSSignpostID {
+        OSSignpostID(UInt64(abs(hashValue)))
+    }
+
+}
+
+// MARK: - Utilities
+
+extension Logger {
     private static func shouldRecordMeasurePass() -> Bool {
         BlueprintLogging.isEnabled &&
             BlueprintLogging.config.recordElementMeasures
     }
+
 }
