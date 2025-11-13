f91cf8f71bf8f1e0b85a127e6694f52d1a0a936b
johnnewman-square <johnnewman@squareup.com>
2025-11-13 12:26:29 -0800

    Adding safe area edge detection for the scrollableAxes behavior (#596)
    Adding a `ScrollView` API to allow clients to specify the edges that
    should be inset for the safe area when using the `scrollableAxes`
    content inset behavior. Before this change, content that landed outside
    the safe area but inside the scroll view bounds was not scrollable when
    using
    [`scrollableAxes`](https://developer.apple.com/documentation/uikit/uiscrollview/contentinsetadjustmentbehavior-swift.enum/scrollableaxes).
    (See linked docs for details)
    
    The goal of this update is to address the issue of content not being
    scrollable without introducing breaking API changes or large behavioral
    changes to ScrollView. A few alternative approaches could be:
    - turn on `alwaysBouncesVertical` and/or `alwaysBounceHorizontal`, which
    will cause `scrollableAxes` to honor the safe areas along both edges of
    the bouncing axes.
    - update client implementations to use the `always` content inset
    behavior. This would require refactoring the ScrollView layout to no
    longer produce a `contentSize` equal to the ScrollView bounds when using
    `fittingWidth` and `fittingHeight`.
    
    The approach in this PR will enable the safe area behavior we need along
    a precise edge, like the bottom, while limiting the risk of regressions.

---
+++ BlueprintUI/Tests/ScrollViewSafeAreaEdgeTests.swift
@@ -0,0 +1,144 @@
+import BlueprintUI
+import XCTest
+@testable import BlueprintUICommonControls
+
+final class ScrollViewSafeAreaEdgeTests: XCTestCase {
+
+    func test_scrollableAxesSafeAreaEdges_givenNoOverlap() throws {
+        try setupScrollView { controller in
+            // No contentInset should be applied because the content is within the safe area.
+            let uiScrollView = try controller.view.expectedChild(ofType: UIScrollView.self)
+            XCTAssertEqual(uiScrollView.contentInset, .zero)
+        }
+    }
+
+    func test_scrollableAxesSafeAreaEdges_givenVerticalOverlap() throws {
+        try setupScrollView { controller in
+            // Decrease the vertical safe area.
+            controller.additionalSafeAreaInsets = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+            controller.view.layoutIfNeeded()
+
+            // The contentInset should be adjusted to reach the safe area.
+            let uiScrollView = try controller.view.expectedChild(ofType: UIScrollView.self)
+            XCTAssertEqual(uiScrollView.contentInset, UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0))
+        }
+    }
+
+    func test_scrollableAxesSafeAreaEdges_givenHorizontalOverlap() throws {
+        try setupScrollView { controller in
+            // Decrease the horizontal safe area.
+            controller.additionalSafeAreaInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+            controller.view.layoutIfNeeded()
+
+            // The contentInset should be adjusted to reach the safe area.
+            let uiScrollView = try controller.view.expectedChild(ofType: UIScrollView.self)
+            XCTAssertEqual(uiScrollView.contentInset, UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10))
+        }
+    }
+
+    func test_scrollableAxesSafeAreaEdges_givenAllOverlap() throws {
+        try setupScrollView { controller in
+            // Decrease the vertical safe area.
+            controller.additionalSafeAreaInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+            controller.view.layoutIfNeeded()
+
+            // The contentInset should be adjusted to reach the safe area.
+            let uiScrollView = try controller.view.expectedChild(ofType: UIScrollView.self)
+            XCTAssertEqual(uiScrollView.contentInset, UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+        }
+    }
+
+    func test_scrollableAxesSafeAreaEdges_givenBottomOverlap() throws {
+        try setupScrollView(scrollableAxesSafeAreaEdges: .bottom) { controller in
+            // Decrease the vertical safe area.
+            controller.additionalSafeAreaInsets = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+            controller.view.layoutIfNeeded()
+
+            // Only the bottom contentInset should be adjusted to reach the safe area.
+            let uiScrollView = try controller.view.expectedChild(ofType: UIScrollView.self)
+            XCTAssertEqual(uiScrollView.contentInset, UIEdgeInsets(top: 0, left: 0, bottom: 10, right: 0))
+        }
+    }
+
+    func test_scrollableAxesSafeAreaEdges_givenRightOverlap() throws {
+        try setupScrollView(scrollableAxesSafeAreaEdges: .right) { controller in
+            // Decrease the horizontal safe area.
+            controller.additionalSafeAreaInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+            controller.view.layoutIfNeeded()
+
+            // Only the right contentInset should be adjusted to reach the safe area.
+            let uiScrollView = try controller.view.expectedChild(ofType: UIScrollView.self)
+            XCTAssertEqual(uiScrollView.contentInset, UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 10))
+        }
+    }
+
+    func test_scrollableAxesSafeAreaEdges_givenOmittedVerticalOverlap() throws {
+        try setupScrollView(scrollableAxesSafeAreaEdges: .horizontal) { controller in
+            // Decrease the safe area on all sides.
+            controller.additionalSafeAreaInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+            controller.view.layoutIfNeeded()
+
+            // Only the horizontal contentInset should be adjusted to reach the safe area.
+            let uiScrollView = try controller.view.expectedChild(ofType: UIScrollView.self)
+            XCTAssertEqual(uiScrollView.contentInset, UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10))
+        }
+    }
+
+    func test_scrollableAxesSafeAreaEdges_givenOmittedHorizontalOverlap() throws {
+        try setupScrollView(scrollableAxesSafeAreaEdges: .vertical) { controller in
+            // Decrease the safe area on all sides.
+            controller.additionalSafeAreaInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+            controller.view.layoutIfNeeded()
+
+            // Only the vertical contentInset should be adjusted to reach the safe area.
+            let uiScrollView = try controller.view.expectedChild(ofType: UIScrollView.self)
+            XCTAssertEqual(uiScrollView.contentInset, UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0))
+        }
+    }
+
+    func test_scrollableAxesSafeAreaEdges_givenOutsideScrollViewBounds() throws {
+        try setupScrollView(testSize: CGSize(width: 9999, height: 9999)) { controller in
+            // No contentInset should be applied because the content is outside the bounds
+            // of the scroll view.
+            let uiScrollView = try controller.view.expectedChild(ofType: UIScrollView.self)
+            XCTAssertEqual(uiScrollView.contentInset, .zero)
+        }
+    }
+
+    func test_scrollableAxesSafeAreaEdges_givenIgnoredSafeArea() throws {
+        try setupScrollView(scrollableAxesSafeAreaEdges: []) { controller in
+            // Decrease the safe area on all sides.
+            controller.additionalSafeAreaInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+            controller.view.layoutIfNeeded()
+
+            // No contentInset should be applied because we're ignoring all safe area edges.
+            let uiScrollView = try controller.view.expectedChild(ofType: UIScrollView.self)
+            XCTAssertEqual(uiScrollView.contentInset, .zero)
+        }
+    }
+
+    func setupScrollView(
+        scrollableAxesSafeAreaEdges: ScrollView.SafeAreaEdge = .all,
+        testSize: CGSize? = nil,
+        _ test: (UIViewController
+        ) throws -> Void
+    ) throws {
+        try show(vc: UIViewController()) { controller in
+            // Pick a frame for the ScrollView that is completely inside the safe area.
+            let scrollViewFrame = controller.view.safeAreaLayoutGuide.layoutFrame
+
+            // Make the content equal to the ScrollView's bounds by default.
+            let content = Box().constrainedTo(size: testSize ?? scrollViewFrame.size)
+            let scrollView = ScrollView(.fittingContent, wrapping: content) { scrollView in
+                scrollView.contentInsetAdjustmentBehavior = .scrollableAxes
+                scrollView.scrollableAxesSafeAreaEdges = scrollableAxesSafeAreaEdges
+            }
+            let containerView = BlueprintView(element: scrollView)
+            containerView.frame = scrollViewFrame
+            controller.view.addSubview(containerView)
+            containerView.layoutIfNeeded()
+
+            try test(controller)
+        }
+    }
+}
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+- Added a `scrollableAxesSafeAreaEdges` property to `ScrollView` that is used with `ContentInsetAdjustmentBehavior.scrollableAxes`.
+
 ### Removed
 
 ### Changed
