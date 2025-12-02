72a7cecf3570572f3e227b12eb1fcc30ebee7846
Max Goedjen <maxg@squareup.com>
2025-12-02 10:34:39 -0800

    Move cache

--- BlueprintUI/Sources/CrossLayoutCaching/Cache/CacheImplementations/ValidatingCache.swift
+++ BlueprintUI/Sources/CrossLayoutCaching/Cache/CacheImplementations/ValidatingCache.swift
@@ -65,41 +65,6 @@ import Foundation
 
 }
 
-/// A convenience wrapper around ValidatingCache which ensures that only values which were cached in equivalent environments are returned, and allows for additional data to be stored to be validated.
-@_spi(HostingViewContext) public struct EnvironmentAndValueValidatingCache<Key, Value, AdditionalValidationData>: Sendable where Key: Hashable {
-
-    private var backing = ValidatingCache<Key, Value, (EnvironmentAccessList, AdditionalValidationData)>()
-
-    public init() {}
-
-    /// Retrieves or creates a value based on a key and a validation function, alongside environment validation.
-    /// - Parameters:
-    ///   - key: The key to look up.
-    ///   - environment: The current environment. A frozen version of this environment will be preserved along with freshly cached values for comparison.
-    ///   - context: The equivalency context in which the environment should be evaluated.
-    ///   - validate: A function that evaluates whether or not a given result is still valid.
-    ///   - create: Creates a fresh cache entry no valid cached data is available, and stores it.
-    /// - Returns: Either a cached or newly created value.
-    /// - Note: Generally, prefer the `validationValue` versions of this method if the validation value conforms to ContextuallyEquivalent or Equatable.
-    mutating func retrieveOrCreate(
-        key: Key,
-        environment: Environment,
-        context: CrossLayoutCacheableContext,
-        validate: (AdditionalValidationData) -> Bool,
-        create: (Environment) -> (Value, AdditionalValidationData)
-    ) -> Value {
-        backing.retrieveOrCreate(key: key) {
-            environment.isCacheablyEquivalent(to: $0.0, in: context) && validate($0.1)
-        } create: {
-            let ((value, additional), accessList) = environment.observingAccess { environment in
-                create(environment)
-            }
-            return (value, (accessList, additional))
-        }
-    }
-
-}
-
 extension Equatable {
 
     fileprivate func isEqual(_ other: any Equatable) -> Bool {
