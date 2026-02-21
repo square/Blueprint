17a658f91806469f78f5ad4bae7218ff2930dc7d
Max Goedjen <maxg@squareup.com>
2026-02-20 18:23:34 -0800

    Temp

--- BlueprintUIAccessibilityCore/Sources/Extensions/Optional+Extensions.swift
+++ BlueprintUIAccessibilityCore/Sources/Extensions/Optional+Extensions.swift
@@ -3,7 +3,7 @@ import Foundation
 extension Optional where Wrapped: RangeReplaceableCollection {
 
     static func + (left: Wrapped?, right: Wrapped?) -> Wrapped? {
-        let val = (left ?? Wrapped()) + (right ?? Wrapped())
+        let val: Wrapped = (left ?? Wrapped()) + (right ?? Wrapped())
         return val.isEmpty ? nil : val
     }
 
